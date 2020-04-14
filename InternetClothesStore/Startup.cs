350f2096bfb605075199d61f89346ff82e03c319
Max <kolotseymax@gmail.com>
2020-04-14 15:07:05 +0300

    Add project files.

---
+++ InternetClothesStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InternetClothesStore.Startup))]
+namespace InternetClothesStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
